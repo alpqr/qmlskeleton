81c73d76df495bb6805d3b8e74c799290ba71b9a
Laszlo Agocs <laszlo.agocs@qt.io>
2021-06-16 14:06:47 +0200

    Update for Qt 6.2

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 int main(int argc, char *argv[])
 {
     qputenv("QSG_INFO", "1");
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
 
     QQuickView view;
